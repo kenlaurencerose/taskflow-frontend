77bafdac28a6336c81b97074e5168d6ee6a9a264
kenlaurencerose <kenlaurencerose@gmail.com>
2026-04-30 10:30:26 +0200

    initial commit

---
+++ src/components/TaskList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Task Liste</h1>
+
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }} - {{ task.status }} - {{ task.priority }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: "Projekt anlegen", status: "Open", priority: "High" },
+        { id: 2, title: "Vue bauen", status: "In Progress", priority: "Medium" },
+        { id: 3, title: "Repo pushen", status: "Done", priority: "High" }
+      ]
+    };
+  }
+};
+</script>
